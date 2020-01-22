e16abdd62f16755a50b6a31369486c14ada15717
cpz <1337@live.it>
2020-01-22 19:59:31 +0300

    Updated to latest version

---
+++ SDK/LA_AkAudio_structs.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+// Lost Ark (1.76.76.0) SDK
+
+#ifdef _MSC_VER
+	#pragma pack(push, 0x4)
+#endif
+
+#include "LA_Basic.hpp"
+#include "LA_Engine_classes.hpp"
+#include "LA_Core_classes.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Script Structs
+//---------------------------------------------------------------------------
+
+// ScriptStruct AkAudio.InterpTrackAkEvent.AkEventTrackKey
+// 0x000C
+struct FAkEventTrackKey
+{
+	float                                              Time;                                                     // 0x0000(0x0004)
+	class UAkEvent*                                    Event;                                                    // 0x0004(0x0008) (Edit)
+};
+
+}
+
+#ifdef _MSC_VER
+	#pragma pack(pop)
+#endif
