bb4fce8ee5a4629ccff74583cce9ca96679d2b94
cpz <1337@live.it>
2020-01-23 00:21:38 +0300

    1.76.76.0

---
+++ SDK/LA_Basic.cpp
@@ -0,0 +1,17 @@
+// Lost Ark (1.61.63.0) SDK
+
+#ifdef _MSC_VER
+	#pragma pack(push, 0x4)
+#endif
+
+#include "../SDK.hpp"
+
+namespace SDK
+{
+TArray<FNameEntry*>* FName::GNames = nullptr;
+TArray<UObject*>* UObject::GObjects = nullptr;
+}
+
+#ifdef _MSC_VER
+	#pragma pack(pop)
+#endif
